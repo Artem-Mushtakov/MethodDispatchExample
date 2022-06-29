5e4fbecf5f985e1619e9b954d85223deb53d48ab
Artem Mushtakov <a.vladimirovich@internet.ru>
2022-06-29 22:13:32 +0400

    Initial Commit

---
+++ MethodDispatchExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MethodDispatchExample
+//
+//  Created by Artem Mushtakov on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
