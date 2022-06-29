2744cc27d310b83d7ff733b744c0c9ee23375194
Artem Mushtakov <a.vladimirovich@internet.ru>
2022-06-29 22:34:27 +0400

    Create hierarchy.

---
+++ MethodDispatchExample/App/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MethodDispatchExample
+//
+//  Created by Artem Mushtakov on 29.06.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
